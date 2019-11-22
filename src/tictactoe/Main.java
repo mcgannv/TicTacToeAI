28898ddbee6e018c195f55109147c15e858e63f2
Vil McGann <mcgannvil@gmail.com>
2019-11-22 12:52:48 -0600

    set up javaFX

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -1,8 +1,22 @@
 package tictactoe;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
-	// write your code here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("tictactoeUI.fxml"));
+        stage.setTitle("TicTacToe");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
